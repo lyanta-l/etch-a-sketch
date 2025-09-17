86f95d7728ef14ee84ed54b972150e7ba3c717eb
lyanta-l <lyanta.why@gmail.com>
2025-09-17 16:57:34 +0800

    feat:给网格添加了悬浮变黑色效果
    使用事件监听器，给触发事件的网格添加.hover类
    在css文件里面添加.hover类的背景颜色

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ root.style.setProperty('--grid-size',gridSize)
 for (let i = 0; i < 24 * 24; i++) {
     const sketch = document.createElement("div");
     sketch.classList.add("sketch");
+    sketch.addEventListener("mouseover",function(e){
+        e.target.classList.add("hover");
+    });
     container.appendChild(sketch);
 }
