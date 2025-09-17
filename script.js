27455dd51d1893a86e84d9b517a393f6d0d8c121
lyanta-l <lyanta.why@gmail.com>
2025-09-17 16:36:20 +0800

    添加初始文件&添加js创建表格功能

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+const root = document.querySelector("html");
+const gridSize = 24;
+root.style.setProperty('--grid-size',gridSize)
+for (let i = 0; i < 24 * 24; i++) {
+    const sketch = document.createElement("div");
+    sketch.classList.add("sketch");
+    container.appendChild(sketch);
+}
