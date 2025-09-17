1f8d99e2fadb4af4f28e7b95933b475e344214eb
lyanta-l <lyanta.why@gmail.com>
2025-09-17 19:26:56 +0800

    feat:从黑色变为随即色
    删除了css文件里的hover类
    使用js修改元素背景色属性，通过rgb和随即数

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const clearButton = document.querySelector("#clear");
 const sizeButton = document.querySelector("#size-btn");
 let gridSize = 24;
 
+const rgbRandom = function () {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return `rgb(${x},${y},${z})`;
+}
+
 const createGrid = function () {
     root.style.setProperty('--grid-size', gridSize)
     for (let i = 0; i < gridSize * gridSize; i++) {
         const sketch = document.createElement("div");
         sketch.classList.add("sketch");
         sketch.addEventListener("mouseover", function (e) {
-            e.target.classList.add("hover");
+            e.target.style.backgroundColor=rgbRandom();
         });
         container.appendChild(sketch);
     }
 }
+
 
 sizeButton.addEventListener("click", () => {
     gridSize = parseInt(prompt("输入size大小"));
@@ -27,6 +35,6 @@ sizeButton.addEventListener("click", () => {
 createGrid();
 clearButton.addEventListener("click", () => {
     document.querySelectorAll(".sketch").forEach(item => {
-        item.classList.remove("hover");
+        item.style.backgroundColor="";
     })
 })
