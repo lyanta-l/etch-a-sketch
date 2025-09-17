b1763ad67930072ed6a1d3d986601890c279d302
lyanta-l <lyanta.why@gmail.com>
2025-09-17 18:59:39 +0800

    feat:增加了用户改变网格数量的功能

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const container = document.querySelector("#container");
 const root = document.querySelector("html");
 const clearButton = document.querySelector("#clear");
-const gridSize = 24;
-root.style.setProperty('--grid-size',gridSize)
-for (let i = 0; i < 24 * 24; i++) {
-    const sketch = document.createElement("div");
-    sketch.classList.add("sketch");
-    sketch.addEventListener("mouseover",function(e){
-        e.target.classList.add("hover");
-    });
-    container.appendChild(sketch);
+const sizeButton = document.querySelector("#size-btn");
+let gridSize = 24;
+
+const createGrid = function () {
+    root.style.setProperty('--grid-size', gridSize)
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const sketch = document.createElement("div");
+        sketch.classList.add("sketch");
+        sketch.addEventListener("mouseover", function (e) {
+            e.target.classList.add("hover");
+        });
+        container.appendChild(sketch);
+    }
 }
-clearButton.addEventListener("click",() => {
+
+sizeButton.addEventListener("click", () => {
+    gridSize = parseInt(prompt("输入size大小"));
+    if (!isNaN(gridSize)) {
+        container.innerHTML = "";
+        createGrid();
+    }
+});
+
+createGrid();
+clearButton.addEventListener("click", () => {
     document.querySelectorAll(".sketch").forEach(item => {
         item.classList.remove("hover");
     })
